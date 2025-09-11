e6f881cf1cb075441a14d059c33fe7c29593ecd1
anunyaOrange <anunyaOrange@proton.me>
2025-09-11 09:52:56 +0700

    add file loader

--- password_cracker.py
+++ password_cracker.py
@@ -1,4 +1,12 @@
 import hashlib
 
 def crack_sha1_hash(hash, use_salts = False):
+    passwords = []
+    salts = []
+    with open('top-10000-passwords.txt', 'r') as file:
+        for line in file:
+            passwords.append(line.strip())
+    with open('known-salts.txt', 'r') as file:
+        for line in file:
+            salts.append(line.strip())
     return True
