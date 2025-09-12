026cdada69c30d8c0ec7d4b61212b1387548dc5f
anunyaOrange <anunyaOrange@proton.me>
2025-09-12 09:52:20 +0700

    has

--- password_cracker.py
+++ password_cracker.py
@@ -9,4 +9,11 @@ def crack_sha1_hash(hash, use_salts = False):
     with open('known-salts.txt', 'r') as file:
         for line in file:
             salts.append(line.strip())
+    for pwd in passwords:
+        hex = hashlib.sha1(pwd.encode('utf-8')).hexdigest()
+        if hex == hash:
+            print(pwd)
     return True
+
+print('xxxxxxxxxxxxxxxxxxxxxxx')
+crack_sha1_hash('b305921a3723cd5d70a375cd21a61e60aabb84ec')
